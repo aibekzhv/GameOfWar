bbb937d4db51074ae6eb055b9fe3d72b14cab07e
Zhumabekov Aibek <aibekzhv@gmail.com>
2019-05-08 11:00:04 -0700

    evenlisteners in JS DOM

--- js/main.js
+++ js/main.js
@@ -1,7 +1,22 @@
 /*----- constants -----*/
 
- 
 /*----- app's state (variables) -----*/
+var suits = ["spades", "hearts", "clubs", "diams"];
+var cardFace = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"];
+var deck = [];
+var players = [[],[]];
+//here i captured the element
+var fightButton = document.querySelector("#btnBattle");
+
+//event listeners
+fightButton.addEventListener('click',battle);
+
+//functions
+function battle() {
+  console.log('works');
+}
+
+
   
   
 
@@ -9,9 +24,9 @@
 
 
   
-/*----- event listeners -----*/
 
 
 
-/*----- functions -----*/
+
+
  
